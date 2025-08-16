3ed5da3c4bbc7c3b9c6dcabdbfd09f8ee3d4dfe8
danoctavian <danoctavian91@gmail.com>
2025-08-16 20:21:05 +0300

    simplify BufferAdmin

--- src/admin/BufferAdmin.sol
+++ src/admin/BufferAdmin.sol
@@ -7,30 +7,16 @@ import {IERC4626} from "lib/openzeppelin-contracts/contracts/interfaces/IERC4626
 
 /// @title BufferAdmin
 /// @notice Contract for managing buffer addresses for a Vault, with role-based access control.
-/// @dev Only addresses with BUFFER_ADMIN_ROLE can add, remove, or reorder buffers.
+/// @dev Only addresses with BUFFER_ADMIN_ROLE can set the current buffer.
 contract BufferAdmin is AccessControl {
-    /// @notice Thrown when trying to add a buffer that already exists.
-    error BufferAlreadyAdded(address buffer);
     /// @notice Thrown when the buffer is not a valid asset in the vault.
     error NotVaultAsset(address buffer);
     /// @notice Thrown when the buffer does not match the ERC4626 asset.
     error ERC4626AssetMismatch(address buffer);
-    /// @notice Thrown when a buffer is not found in the mapping.
-    error BufferNotFound(address buffer);
-    /// @notice Thrown when a buffer is not found in the array.
-    error BufferNotFoundInArray(address buffer);
-    /// @notice Thrown when the input length does not match the expected length.
-    error LengthMismatch(uint256 expected, uint256 actual);
-    /// @notice Thrown when a duplicate buffer is detected.
-    error DuplicateBuffer(address buffer);
-    /// @notice Thrown when a buffer is not in the list.
-    error BufferNotInList(address buffer);
     /// @notice Thrown when setting the buffer fails.
     error SetBufferFailed();
 
     IVault public vault;
-    address[] public buffers;
-    mapping(address => bool) public isBuffer;
 
     /// @notice Role identifier for buffer admins.
     bytes32 public constant BUFFER_ADMIN_ROLE = keccak256("BUFFER_ADMIN_ROLE");
@@ -45,88 +31,17 @@ contract BufferAdmin is AccessControl {
         _grantRole(BUFFER_ADMIN_ROLE, bufferAdmin);
     }
 
-    /// @notice Add new buffer addresses to the list.
-    /// @dev Only callable by BUFFER_ADMIN_ROLE. Checks for duplicates and asset validity.
-    /// @param _buffers Array of buffer addresses to add.
-    function addBuffers(address[] memory _buffers) public onlyRole(BUFFER_ADMIN_ROLE) {
-        // Check for duplicates in the input array
-        for (uint256 i = 0; i < _buffers.length; i++) {
-            for (uint256 j = i + 1; j < _buffers.length; j++) {
-                if (_buffers[i] == _buffers[j]) revert DuplicateBuffer(_buffers[i]);
-            }
-        }
-        for (uint256 i = 0; i < _buffers.length; i++) {
-            address buf = _buffers[i];
-            if (_isInBuffers(buf)) revert BufferAlreadyAdded(buf);
-            if (!_isVaultAsset(buf)) revert NotVaultAsset(buf);
-            if (!_isERC4626Asset(buf)) revert ERC4626AssetMismatch(buf);
-            buffers.push(buf);
-            isBuffer[buf] = true;
-        }
-    }
-
-    /// @notice Remove buffer addresses from the list.
-    /// @dev Only callable by BUFFER_ADMIN_ROLE.
-    /// @param _buffers Array of buffer addresses to remove.
-    function removeBuffers(address[] memory _buffers) public onlyRole(BUFFER_ADMIN_ROLE) {
-        for (uint256 i = 0; i < _buffers.length; i++) {
-            _removeBuffer(_buffers[i]);
-        }
-    }
-
-    /// @notice Internal function to remove a buffer address.
-    /// @dev Reverts if the buffer is not found.
-    /// @param _buffer The buffer address to remove.
-    function _removeBuffer(address _buffer) internal {
-        if (!isBuffer[_buffer]) revert BufferNotFound(_buffer);
-        uint256 len = buffers.length;
-        for (uint256 i = 0; i < len; i++) {
-            if (buffers[i] == _buffer) {
-                buffers[i] = buffers[len - 1];
-                buffers.pop();
-                isBuffer[_buffer] = false;
-                return;
-            }
-        }
-        revert BufferNotFoundInArray(_buffer);
-    }
-
-    /// @notice Update the order of buffer addresses.
-    /// @dev Only callable by BUFFER_ADMIN_ROLE. Checks for set equality and duplicates.
-    /// @param _buffers The new ordered array of buffer addresses.
-    function updateBufferOrder(address[] memory _buffers) public onlyRole(BUFFER_ADMIN_ROLE) {
-        if (_buffers.length != buffers.length) revert LengthMismatch(buffers.length, _buffers.length);
-        // Check that the set is the same
-        for (uint256 i = 0; i < _buffers.length; i++) {
-            if (!isBuffer[_buffers[i]]) revert BufferNotFound(_buffers[i]);
-        }
-        // Check for duplicates
-        for (uint256 i = 0; i < _buffers.length; i++) {
-            for (uint256 j = i + 1; j < _buffers.length; j++) {
-                if (_buffers[i] == _buffers[j]) revert DuplicateBuffer(_buffers[i]);
-            }
-        }
-        buffers = _buffers;
-    }
-
     /// @notice Set the current buffer in the vault.
-    /// @dev Only callable by BUFFER_ADMIN_ROLE. Checks buffer validity.
+    /// @dev Only callable by BUFFER_ADMIN_ROLE. Performs all validation here.
     /// @param _buffer The buffer address to set as current.
     function setCurrentBuffer(address _buffer) public onlyRole(BUFFER_ADMIN_ROLE) {
-        if (!isBuffer[_buffer]) revert BufferNotInList(_buffer);
-
-        // Check again; buffer may be removed as an asset in the meantime.
+        // Check that _buffer is a valid vault asset
         if (!_isVaultAsset(_buffer)) revert NotVaultAsset(_buffer);
+
+        // Check that _buffer is a valid ERC4626 asset for the vault
+        if (!_isERC4626Asset(_buffer)) revert ERC4626AssetMismatch(_buffer);
+
         vault.setBuffer(_buffer);
-    }
-
-    // --- Internal helpers ---
-
-    /// @notice Checks if an address is in the buffer list.
-    /// @param _buffer The address to check.
-    /// @return True if the address is a buffer, false otherwise.
-    function _isInBuffers(address _buffer) public view returns (bool) {
-        return isBuffer[_buffer];
     }
 
     /// @notice Checks if an address is a valid vault asset.
--- test/unit/bufferadmin.t.sol
+++ test/unit/bufferadmin.t.sol
@@ -56,11 +56,9 @@ contract BufferAdminUnitTest is Test {
 
     address asset1 = address(0x1001);
     address asset2 = address(0x1002);
-    address asset3 = address(0x1003);
 
     ERC4626Mock erc4626_1;
     ERC4626Mock erc4626_2;
-    ERC4626Mock erc4626_3;
 
     function setUp() public {
         vault = new VaultMock();
@@ -69,188 +67,32 @@ contract BufferAdminUnitTest is Test {
         // Set up ERC4626 mocks with correct asset
         erc4626_1 = new ERC4626Mock(asset1);
         erc4626_2 = new ERC4626Mock(asset1);
-        erc4626_3 = new ERC4626Mock(asset1);
 
         // Set up vault assets
         vault.setAsset(address(erc4626_1), 18);
         vault.setAsset(address(erc4626_2), 18);
-        vault.setAsset(address(erc4626_3), 18);
 
         bufferAdmin = new BufferAdmin(address(vault), admin, bufferAdminRole);
     }
 
-    function testAddBuffers() public {
-        vm.prank(bufferAdminRole);
-        address[] memory bufs = new address[](2);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_2);
-        bufferAdmin.addBuffers(bufs);
-
-        assertTrue(bufferAdmin.isBuffer(address(erc4626_1)));
-        assertTrue(bufferAdmin.isBuffer(address(erc4626_2)));
-        assertEq(bufferAdmin.buffers(0), address(erc4626_1));
-        assertEq(bufferAdmin.buffers(1), address(erc4626_2));
-    }
-
-    function testAddBufferRevertsIfDuplicateInInput() public {
-        vm.prank(bufferAdminRole);
-        address[] memory bufs = new address[](2);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_1);
-        vm.expectRevert(BufferAdmin.DuplicateBuffer.selector);
-        bufferAdmin.addBuffers(bufs);
-    }
-
-    function testAddBufferRevertsIfAlreadyAdded() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](1);
-        bufs[0] = address(erc4626_1);
-        bufferAdmin.addBuffers(bufs);
-
-        address[] memory bufs2 = new address[](1);
-        bufs2[0] = address(erc4626_1);
-        vm.expectRevert(BufferAdmin.BufferAlreadyAdded.selector);
-        bufferAdmin.addBuffers(bufs2);
-        vm.stopPrank();
-    }
-
-    function testAddBufferRevertsIfNotVaultAsset() public {
-        vm.prank(bufferAdminRole);
-        address fake = address(0xdeadbeef);
-        address[] memory bufs = new address[](1);
-        bufs[0] = fake;
-        vm.expectRevert(BufferAdmin.NotVaultAsset.selector);
-        bufferAdmin.addBuffers(bufs);
-    }
-
-    function testAddBufferRevertsIfERC4626AssetMismatch() public {
-        // ERC4626Mock with wrong asset
-        ERC4626Mock wrongERC4626 = new ERC4626Mock(asset2);
-        vault.setAsset(address(wrongERC4626), 18);
-
-        vm.prank(bufferAdminRole);
-        address[] memory bufs = new address[](1);
-        bufs[0] = address(wrongERC4626);
-        vm.expectRevert(BufferAdmin.ERC4626AssetMismatch.selector);
-        bufferAdmin.addBuffers(bufs);
-    }
-
-    function testRemoveBuffers() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](2);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_2);
-        bufferAdmin.addBuffers(bufs);
-
-        address[] memory removeBufs = new address[](1);
-        removeBufs[0] = address(erc4626_1);
-        bufferAdmin.removeBuffers(removeBufs);
-
-        assertFalse(bufferAdmin.isBuffer(address(erc4626_1)));
-        assertTrue(bufferAdmin.isBuffer(address(erc4626_2)));
-        vm.stopPrank();
-    }
-
-    function testRemoveBufferRevertsIfNotFound() public {
-        vm.prank(bufferAdminRole);
-        address[] memory bufs = new address[](1);
-        bufs[0] = address(erc4626_1);
-        vm.expectRevert(BufferAdmin.BufferNotFound.selector);
-        bufferAdmin.removeBuffers(bufs);
-    }
-
-    function testUpdateBufferOrder() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](3);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_2);
-        bufs[2] = address(erc4626_3);
-        bufferAdmin.addBuffers(bufs);
-
-        address[] memory newOrder = new address[](3);
-        newOrder[0] = address(erc4626_3);
-        newOrder[1] = address(erc4626_2);
-        newOrder[2] = address(erc4626_1);
-        bufferAdmin.updateBufferOrder(newOrder);
-
-        assertEq(bufferAdmin.buffers(0), address(erc4626_3));
-        assertEq(bufferAdmin.buffers(1), address(erc4626_2));
-        assertEq(bufferAdmin.buffers(2), address(erc4626_1));
-        vm.stopPrank();
-    }
-
-    function testUpdateBufferOrderRevertsIfLengthMismatch() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](2);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_2);
-        bufferAdmin.addBuffers(bufs);
-
-        address[] memory newOrder = new address[](1);
-        newOrder[0] = address(erc4626_1);
-        vm.expectRevert(BufferAdmin.LengthMismatch.selector);
-        bufferAdmin.updateBufferOrder(newOrder);
-        vm.stopPrank();
-    }
-
-    function testUpdateBufferOrderRevertsIfNotInSet() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](2);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_2);
-        bufferAdmin.addBuffers(bufs);
-
-        address[] memory newOrder = new address[](2);
-        newOrder[0] = address(erc4626_1);
-        newOrder[1] = address(erc4626_3); // not in set
-        vm.expectRevert(BufferAdmin.BufferNotFound.selector);
-        bufferAdmin.updateBufferOrder(newOrder);
-        vm.stopPrank();
-    }
-
-    function testUpdateBufferOrderRevertsIfDuplicate() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](2);
-        bufs[0] = address(erc4626_1);
-        bufs[1] = address(erc4626_2);
-        bufferAdmin.addBuffers(bufs);
-
-        address[] memory newOrder = new address[](2);
-        newOrder[0] = address(erc4626_1);
-        newOrder[1] = address(erc4626_1);
-        vm.expectRevert(BufferAdmin.DuplicateBuffer.selector);
-        bufferAdmin.updateBufferOrder(newOrder);
-        vm.stopPrank();
-    }
-
     function testSetCurrentBuffer() public {
         vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](1);
-        bufs[0] = address(erc4626_1);
-        bufferAdmin.addBuffers(bufs);
 
+        // Valid buffer
         bufferAdmin.setCurrentBuffer(address(erc4626_1));
         assertEq(vault.currentBuffer(), address(erc4626_1));
-        vm.stopPrank();
-    }
 
-    function testSetCurrentBufferRevertsIfNotInList() public {
-        vm.prank(bufferAdminRole);
-        vm.expectRevert(BufferAdmin.BufferNotInList.selector);
-        bufferAdmin.setCurrentBuffer(address(erc4626_1));
-    }
-
-    function testSetCurrentBufferRevertsIfNotVaultAsset() public {
-        vm.startPrank(bufferAdminRole);
-        address[] memory bufs = new address[](1);
-        bufs[0] = address(erc4626_1);
-        bufferAdmin.addBuffers(bufs);
-
-        // Remove asset from vault
+        // Revert if not vault asset
         vault.setAsset(address(erc4626_1), 0);
-
         vm.expectRevert(BufferAdmin.NotVaultAsset.selector);
         bufferAdmin.setCurrentBuffer(address(erc4626_1));
+
+        // Revert if ERC4626 asset mismatch
+        ERC4626Mock wrongERC4626 = new ERC4626Mock(asset2);
+        vault.setAsset(address(wrongERC4626), 18);
+        vm.expectRevert(BufferAdmin.ERC4626AssetMismatch.selector);
+        bufferAdmin.setCurrentBuffer(address(wrongERC4626));
+
         vm.stopPrank();
     }
 }

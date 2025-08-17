ce0dae9168f811aa19db3ee05ffb3d46f24cdf99
danoctavian <danoctavian91@gmail.com>
2025-08-17 17:08:06 +0300

    -wip MetaHooks

--- src/hooks/MetaHooks.sol
+++ src/hooks/MetaHooks.sol
@@ -1,22 +1,51 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.24;
 
-import {IHooks} from "src/interface/IHooks.sol";
+import {IHooks} from "lib/yieldnest-vault/src/interface/IHooks.sol";
 import {IVault} from "lib/yieldnest-vault/src/interface/IVault.sol";
+import {HooksLib} from "lib/yieldnest-vault/src/library/HooksLib.sol";
 
 contract MetaHooks is IHooks {
-    IVault public immutable override VAULT;
+    error ZeroVaultAddress();
+    error DuplicateInInput();
+    error DuplicateWithExistingHook();
 
+    IVault public immutable override VAULT;
+    IHooks[] public hooks;
     Config private _config;
 
     constructor(address vault_) {
-        require(vault_ != address(0), "MetaHooks: vault is zero address");
+        if (vault_ == address(0)) revert ZeroVaultAddress();
         VAULT = IVault(vault_);
     }
 
+    function setHooks(IHooks[] memory hooks_) external {
+        Config memory newConfig;
+        // Check for duplicates in hooks_
+        for (uint256 i = 0; i < hooks_.length; i++) {
+            for (uint256 j = i + 1; j < hooks_.length; j++) {
+                if (hooks_[i] == hooks_[j]) revert DuplicateInInput();
+            }
+        }
 
-    function addHooks(address[] memory hooks_) external {
+        // Clear the hooks array before setting new hooks
+        delete hooks;
 
+        for (uint256 i = 0; i < hooks_.length; i++) {
+            hooks.push(hooks_[i]);
+
+            IHooks.Config memory config = hooks_[i].getConfig();
+
+            newConfig.beforeDeposit = newConfig.beforeDeposit || config.beforeDeposit;
+            newConfig.afterDeposit = newConfig.afterDeposit || config.afterDeposit;
+            newConfig.beforeMint = newConfig.beforeMint || config.beforeMint;
+            newConfig.afterMint = newConfig.afterMint || config.afterMint;
+            newConfig.beforeRedeem = newConfig.beforeRedeem || config.beforeRedeem;
+            newConfig.afterRedeem = newConfig.afterRedeem || config.afterRedeem;
+            newConfig.beforeWithdraw = newConfig.beforeWithdraw || config.beforeWithdraw;
+        }
+
+        setConfig(newConfig);
     }
 
     modifier onlyVault() {
@@ -24,102 +53,108 @@ contract MetaHooks is IHooks {
         _;
     }
 
-
-
-    function setConfig(Config memory config_) external override {
+    function setConfig(Config memory config_) public override {
         _config = config_;
     }
 
-    function getConfig() external view override returns (Config memory) {
+    function getConfig() public view override returns (Config memory) {
         return _config;
     }
 
-    function beforeDeposit(
-        uint256 assets,
-        address caller,
-        address receiver,
-        uint256 shares,
-        uint256 baseAssets
-    ) external override onlyVault {
+    function beforeDeposit(uint256 assets, address caller, address receiver, uint256 shares, uint256 baseAssets)
+        external
+        override
+        onlyVault
+    {
         if (!_config.beforeDeposit) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.beforeDeposit(hooks[i], assets, caller, receiver, shares, baseAssets);
+        }
     }
 
-    function afterDeposit(
-        uint256 assets,
-        address caller,
-        address receiver,
-        uint256 shares,
-        uint256 baseAssets
-    ) external override onlyVault {
+    function afterDeposit(uint256 assets, address caller, address receiver, uint256 shares, uint256 baseAssets)
+        external
+        override
+        onlyVault
+    {
         if (!_config.afterDeposit) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.afterDeposit(hooks[i], assets, caller, receiver, shares, baseAssets);
+        }
     }
 
-    function beforeMint(
-        uint256 shares,
-        address caller,
-        address receiver,
-        uint256 assets,
-        uint256 baseAssets
-    ) external override onlyVault {
+    function beforeMint(uint256 shares, address caller, address receiver, uint256 assets, uint256 baseAssets)
+        external
+        override
+        onlyVault
+    {
         if (!_config.beforeMint) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.beforeMint(hooks[i], shares, caller, receiver, assets, baseAssets);
+        }
     }
 
-    function afterMint(
-        uint256 shares,
-        address caller,
-        address receiver,
-        uint256 assets,
-        uint256 baseAssets
-    ) external override onlyVault {
+    function afterMint(uint256 shares, address caller, address receiver, uint256 assets, uint256 baseAssets)
+        external
+        override
+        onlyVault
+    {
         if (!_config.afterMint) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.afterMint(hooks[i], shares, caller, receiver, assets, baseAssets);
+        }
     }
 
-    function beforeRedeem(
-        uint256 shares,
-        address caller,
-        address receiver,
-        address owner,
-        uint256 assets
-    ) external override onlyVault {
+    function beforeRedeem(uint256 shares, address caller, address receiver, address owner, uint256 assets)
+        external
+        override
+        onlyVault
+    {
         if (!_config.beforeRedeem) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.beforeRedeem(hooks[i], shares, caller, receiver, owner, assets);
+        }
     }
 
-    function afterRedeem(
-        uint256 shares,
-        address caller,
-        address receiver,
-        address owner,
-        uint256 assets
-    ) external override onlyVault {
+    function afterRedeem(uint256 shares, address caller, address receiver, address owner, uint256 assets)
+        external
+        override
+        onlyVault
+    {
         if (!_config.afterRedeem) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.afterRedeem(hooks[i], shares, caller, receiver, owner, assets);
+        }
     }
 
-    function beforeWithdraw(
-        uint256 assets,
-        address caller,
-        address receiver,
-        address owner,
-        uint256 shares
-    ) external override onlyVault {
+    function beforeWithdraw(uint256 assets, address caller, address receiver, address owner, uint256 shares)
+        external
+        override
+        onlyVault
+    {
         if (!_config.beforeWithdraw) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.beforeWithdraw(hooks[i], assets, caller, receiver, owner, shares);
+        }
     }
 
-    function afterWithdraw(
-        uint256 assets,
-        address caller,
-        address receiver,
-        address owner,
-        uint256 shares
-    ) external override onlyVault {
+    function afterWithdraw(uint256 assets, address caller, address receiver, address owner, uint256 shares)
+        external
+        override
+        onlyVault
+    {
         if (!_config.afterWithdraw) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.afterWithdraw(hooks[i], assets, caller, receiver, owner, shares);
+        }
     }
 
     function beforeProcessAccounting(
@@ -128,7 +163,12 @@ contract MetaHooks is IHooks {
         uint256 totalBaseBalanceBeforeAccounting
     ) external override onlyVault {
         if (!_config.beforeProcessAccounting) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.beforeProcessAccounting(
+                hooks[i], totalAssetsBeforeAccounting, totalSupplyBeforeAccounting, totalBaseBalanceBeforeAccounting
+            );
+        }
     }
 
     function afterProcessAccounting(
@@ -140,7 +180,36 @@ contract MetaHooks is IHooks {
         uint256 totalBaseBalanceBeforeAccounting
     ) external override onlyVault {
         if (!_config.afterProcessAccounting) return;
-        // Custom logic can be implemented here
+
+        for (uint256 i = 0; i < hooks.length; i++) {
+            HooksLib.afterProcessAccounting(
+                hooks[i],
+                totalAssetsBeforeAccounting,
+                totalAssetsAfterAccounting,
+                totalSupplyBeforeAccounting,
+                totalSupplyAfterAccounting,
+                totalBaseBalanceAfterAccounting,
+                totalBaseBalanceBeforeAccounting
+            );
+        }
     }
 
+    // TODO: remove when interface is simplified
+
+    function performanceFee() external pure returns (uint256) {
+        // stub
+        return 0;
+    }
+
+    function performanceFeeRecipient() external pure returns (address) {
+        // stub
+        return address(0);
+    }
+
+    function setPerformanceFee(uint256 /*performanceFee_*/ ) external pure {
+        // stub
+    }
+    function setPerformanceFeeRecipient(address /*performanceFeeRecipient_*/ ) external pure {
+        // stub
+    }
 }

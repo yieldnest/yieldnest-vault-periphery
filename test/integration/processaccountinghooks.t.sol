e8dc94c8351bc46cd7c5b153bc0c629529932bb3
danoctavian <danoctavian91@gmail.com>
2025-08-28 19:38:12 +0300

    add processAccounting failure test

--- test/integration/processaccountinghooks.t.sol
+++ test/integration/processaccountinghooks.t.sol
@@ -11,6 +11,7 @@ import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.so
 import {PermissionedVaultHook} from "test/testhooks/PermissionedVaultHook.sol";
 import {HooksLib, HookCallFailed} from "lib/yieldnest-vault/src/library/HooksLib.sol";
 import {ProcessorUtils} from "lib/yieldnest-vault/test/utils/ProcessorUtils.sol";
+import {ProcessAccountingGuardHook} from "src/hooks/ProcessAccountingGuardHook.sol";
 
 // Minimal mock for IHooks
 contract ProcessAccountingHooksIntegrationTest is BaseIntegrationTest {
@@ -32,6 +33,41 @@ contract ProcessAccountingHooksIntegrationTest is BaseIntegrationTest {
 
         // Process accounting should succeed (within allowed ratio bounds)
         vm.startPrank(PROCESSOR);
+        vault.processAccounting();
+        vm.stopPrank();
+    }
+
+    function test_deposit_donate_and_processAccounting_revert() public {
+        // Deposit as whitelisted user
+        deal(vault.asset(), depositor, 100 ether);
+        vm.startPrank(depositor);
+        IERC20(vault.asset()).approve(address(vault), 100 ether);
+        vault.deposit(100 ether, depositor);
+        vm.stopPrank();
+        // Verify deposit was successful
+        assertEq(vault.balanceOf(depositor), 100 ether);
+        uint256 totalAssetsBefore = vault.totalAssets();
+        assertEq(totalAssetsBefore, 100 ether);
+
+        // Donate a large amount to trigger the increase ratio guard
+        // This will cause totalAssets to increase significantly without corresponding shares
+        uint256 donationAmount = 10 ether;
+        deal(vault.asset(), address(this), donationAmount);
+        IERC20(vault.asset()).transfer(address(vault), donationAmount);
+
+        uint256 totalAssetsAfter = totalAssetsBefore + donationAmount;
+        uint256 maxIncreaseRatio = processAccountingGuardHook.maxIncreaseRatio();
+
+        // Process accounting should revert due to exceeding maxIncreaseRatio (0.2%)
+        // The donation increased assets by 10% which is way above the 0.2% limit
+        vm.startPrank(PROCESSOR);
+        bytes memory revertData = abi.encodeWithSelector(
+            ProcessAccountingGuardHook.TotalAssetsIncreasedTooMuch.selector,
+            totalAssetsBefore,
+            totalAssetsAfter,
+            maxIncreaseRatio
+        );
+        vm.expectRevert(abi.encodeWithSelector(HookCallFailed.selector, revertData));
         vault.processAccounting();
         vm.stopPrank();
     }

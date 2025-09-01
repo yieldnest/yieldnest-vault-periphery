984dcbc3f37d9de20c1873f09415f94590cf9ee9
danoctavian <danoctavian91@gmail.com>
2025-09-01 18:57:55 +0300

    better tests for calling and setting hooks

--- test/local/unit/metahooks/setHooks.t.sol
+++ test/local/unit/metahooks/setHooks.t.sol
@@ -20,57 +20,78 @@ contract MetaHooksTest is Test {
         metaHooks = new MetaHooks(address(vault), admin, hookManager);
     }
 
-    function testSetHooksAndConfig() public {
+    function test_setHooks_success(
+        bool[5] memory beforeDeposits,
+        bool[5] memory afterDeposits,
+        bool[5] memory beforeMints,
+        bool[5] memory afterMints,
+        bool[5] memory beforeRedeems,
+        bool[5] memory afterRedeems,
+        bool[5] memory beforeWithdraws,
+        bool[5] memory afterWithdraws,
+        bool[5] memory beforeProcessAccountings,
+        bool[5] memory afterProcessAccountings
+    ) public {
         vm.startPrank(hookManager);
+        uint256 numHooks = 5;
 
-        // Set up two hooks with different configs
-        IHooks.Config memory config1 = IHooks.Config({
-            beforeDeposit: true,
-            afterDeposit: false,
-            beforeMint: false,
-            afterMint: false,
-            beforeRedeem: false,
-            afterRedeem: false,
-            beforeWithdraw: false,
-            afterWithdraw: false,
-            beforeProcessAccounting: false,
-            afterProcessAccounting: false
-        });
-        IHooks.Config memory config2 = IHooks.Config({
-            beforeDeposit: false,
-            afterDeposit: true,
-            beforeMint: false,
-            afterMint: false,
-            beforeRedeem: false,
-            afterRedeem: false,
-            beforeWithdraw: false,
-            afterWithdraw: false,
-            beforeProcessAccounting: false,
-            afterProcessAccounting: false
-        });
+        // Create arrays of 5 hooks with fuzzed configurations
+        HooksMock[] memory hooks = new HooksMock[](numHooks);
 
-        HooksMock hook1 = new HooksMock(config1);
-        HooksMock hook2 = new HooksMock(config2);
+        // Initialize hooks with fuzzed configurations
+        for (uint256 i = 0; i < numHooks; i++) {
+            hooks[i] = new HooksMock(
+                IHooks.Config({
+                    beforeDeposit: beforeDeposits[i],
+                    afterDeposit: afterDeposits[i],
+                    beforeMint: beforeMints[i],
+                    afterMint: afterMints[i],
+                    beforeRedeem: beforeRedeems[i],
+                    afterRedeem: afterRedeems[i],
+                    beforeWithdraw: beforeWithdraws[i],
+                    afterWithdraw: afterWithdraws[i],
+                    beforeProcessAccounting: beforeProcessAccountings[i],
+                    afterProcessAccounting: afterProcessAccountings[i]
+                })
+            );
+        }
 
-        IHooks[] memory hooksArr = new IHooks[](2);
-        hooksArr[0] = IHooks(address(hook1));
-        hooksArr[1] = IHooks(address(hook2));
+        IHooks[] memory hooksArr = new IHooks[](numHooks);
+        for (uint256 i = 0; i < numHooks; i++) {
+            hooksArr[i] = IHooks(address(hooks[i]));
+        }
 
         metaHooks.setHooks(hooksArr);
 
         // Config should be the OR of all hooks
         IHooks.Config memory config = metaHooks.getConfig();
-        assertTrue(config.beforeDeposit, "beforeDeposit should be true");
-        assertTrue(config.afterDeposit, "afterDeposit should be true");
-        assertFalse(config.beforeMint, "beforeMint should be false");
+
+        assertHookToggle(config.beforeDeposit, beforeDeposits);
+        assertHookToggle(config.afterDeposit, afterDeposits);
+        assertHookToggle(config.beforeMint, beforeMints);
+        assertHookToggle(config.afterMint, afterMints);
+        assertHookToggle(config.beforeRedeem, beforeRedeems);
+        assertHookToggle(config.afterRedeem, afterRedeems);
+        assertHookToggle(config.beforeWithdraw, beforeWithdraws);
+        assertHookToggle(config.afterWithdraw, afterWithdraws);
+        assertHookToggle(config.beforeProcessAccounting, beforeProcessAccountings);
+        assertHookToggle(config.afterProcessAccounting, afterProcessAccountings);
 
         // Should revert on duplicate hooks
         IHooks[] memory dupArr = new IHooks[](2);
-        dupArr[0] = IHooks(address(hook1));
-        dupArr[1] = IHooks(address(hook1));
-        vm.expectRevert(abi.encodeWithSelector(MetaHooks.DuplicateInInput.selector, address(hook1)));
+        dupArr[0] = IHooks(address(hooks[0]));
+        dupArr[1] = IHooks(address(hooks[0]));
+        vm.expectRevert(abi.encodeWithSelector(MetaHooks.DuplicateInInput.selector, address(hooks[0])));
         metaHooks.setHooks(dupArr);
 
         vm.stopPrank();
+    }
+
+    function assertHookToggle(bool configValue, bool[5] memory hookIsActive) internal {
+        bool expected = false;
+        for (uint256 i = 0; i < 5; i++) {
+            expected = expected || hookIsActive[i];
+        }
+        assertEq(configValue, expected, "configValue should match OR of all hooks");
     }
 }

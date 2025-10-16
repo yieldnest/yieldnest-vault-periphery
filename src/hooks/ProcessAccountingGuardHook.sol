be31b7722dc96a1444b736a31135f2c24b388d2e
danoctavian <danoctavian91@gmail.com>
2025-10-16 21:04:18 +0300

    fix version var name

--- src/hooks/ProcessAccountingGuardHook.sol
+++ src/hooks/ProcessAccountingGuardHook.sol
@@ -17,7 +17,7 @@ import {Math} from "lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
 contract ProcessAccountingGuardHook is IHooks {
     using Math for uint256;
 
-    string public constant STRATEGY_VERSION = "0.1.1";
+    string public constant VERSION = "0.1.1";
 
     error TotalAssetsDecreasedTooMuch(
         uint256 totalAssetsBefore, uint256 totalAssetsAfter, uint256 maxTotalAssetsDecreaseRatio
